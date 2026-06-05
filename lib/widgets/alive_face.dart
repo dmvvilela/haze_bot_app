63fe647a8bce7ea9ae686cbd7011844eea805051
Daniel Vilela <dmvvilela@gmail.com>
2026-06-05 20:03:37 -0300

    Make Haze V2 spark tufts intentional

--- lib/widgets/alive_face.dart
+++ lib/widgets/alive_face.dart
@@ -102,7 +102,14 @@ class _AliveFacePainter extends CustomPainter {
     _drawEyes(canvas, size, eyeColor, mouthColor, expression, bodyLift);
     _drawBrows(canvas, size, eyeColor, expression, bodyLift);
     _drawMouth(canvas, size, mouthColor, expression, bodyLift);
-    _drawExpressionAccents(canvas, size, eyeColor, mouthColor, expression);
+    _drawExpressionAccents(
+      canvas,
+      size,
+      eyeColor,
+      mouthColor,
+      expression,
+      bodyLift,
+    );
   }
 
   void _drawGlow(
@@ -697,6 +704,7 @@ class _AliveFacePainter extends CustomPainter {
     Color eyeColor,
     Color mouthColor,
     RobotExpression expression,
+    double bodyLift,
   ) {
     switch (expression) {
       case RobotExpression.sleepy:
@@ -704,7 +712,7 @@ class _AliveFacePainter extends CustomPainter {
         break;
       case RobotExpression.excited:
       case RobotExpression.surprised:
-        _drawSignalMarks(canvas, size, eyeColor);
+        _drawSparkTufts(canvas, size, eyeColor, bodyLift);
         break;
       case RobotExpression.love:
         _drawHeart(
@@ -739,22 +747,30 @@ class _AliveFacePainter extends CustomPainter {
     }
   }
 
-  void _drawSignalMarks(Canvas canvas, Size size, Color color) {
+  void _drawSparkTufts(Canvas canvas, Size size, Color color, double bodyLift) {
     final paint = Paint()
-      ..color = color.withValues(alpha: 0.42)
-      ..strokeWidth = 3
+      ..color = color.withValues(alpha: 0.34 + energy * 0.08)
+      ..strokeWidth = 2.6
       ..strokeCap = StrokeCap.round;
-    for (var i = 0; i < 4; i++) {
-      final angle = -math.pi / 2 + (i - 1.5) * 0.28;
-      final start = Offset(
-        size.width / 2 + math.cos(angle) * 84,
-        size.height * 0.25 + math.sin(angle) * 32,
+
+    final tufts = [
+      (baseX: 0.36, lean: -0.7, length: 18.0),
+      (baseX: 0.42, lean: -0.35, length: 16.0),
+      (baseX: 0.58, lean: 0.32, length: 16.0),
+      (baseX: 0.65, lean: 0.72, length: 18.0),
+    ];
+    for (var i = 0; i < tufts.length; i++) {
+      final tuft = tufts[i];
+      final wiggle = math.sin(time * 5 + i) * 2.5;
+      final base = Offset(
+        size.width * tuft.baseX,
+        size.height * 0.17 + bodyLift,
       );
-      final end = Offset(
-        size.width / 2 + math.cos(angle) * (98 + math.sin(time * 4 + i) * 3),
-        size.height * 0.25 + math.sin(angle) * 42,
+      final end = base.translate(
+        tuft.lean * 12 + wiggle,
+        -tuft.length - math.cos(time * 4 + i) * 2,
       );
-      canvas.drawLine(start, end, paint);
+      canvas.drawLine(base, end, paint);
     }
   }
 
